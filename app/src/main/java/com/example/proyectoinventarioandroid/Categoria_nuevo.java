fb11b1870b6e38ce8a140810b63042392ff5f868
ArxAiden <alexmansharinganwolf@gmail.com>
2022-11-17 00:39:53 -0300

    sda

--- app/src/main/java/com/example/proyectoinventarioandroid/Categoria_nuevo.java
+++ app/src/main/java/com/example/proyectoinventarioandroid/Categoria_nuevo.java
@@ -3,8 +3,16 @@ package com.example.proyectoinventarioandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Categoria_nuevo extends AppCompatActivity {
+
+    public static ArrayList<Categorias> listaCategoria=new ArrayList<>();
+    public ArrayList<String> listaNombresCategoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/proyectoinventarioandroid/Categorias.java
+++ app/src/main/java/com/example/proyectoinventarioandroid/Categorias.java
@@ -3,12 +3,35 @@ package com.example.proyectoinventarioandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.proyectoinventarioandroid.models.EstructuraCrear;
+
+import java.util.ArrayList;
 
 public class Categorias extends AppCompatActivity {
+
+    public static ArrayList<Categorias> listaCategoria=new ArrayList<>();
+    public ArrayList<String> listaNombresCategoria;
+    public ListView lViewFCat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorias);
+    }
+
+    public void cargarListView(){
+        final EstructuraCrear db = new EstructuraCrear(getApplicationContext());
+        listaNombresCategoria=new ArrayList<>();
+        listaCategoria=db.reCategoria();
+        lViewFCat = findViewById(R.id.lViewFCat);
+        for(Categorias r:listaCategoria){
+            listaNombresCategoria.add(r.getCodcategoria()+" -- "+r.getNombrecategoria());
+        }
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,listaNombresCategoria);
+        lViewFCat.setAdapter(adapter);
+
     }
 }
